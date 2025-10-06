609f0753d94f448bfcdfba8dce4aee1debd981ee
harisnaseer24 <harisnaseer258@gmail.com>
2025-10-06 12:48:19 +0500

    route parameters,query parameters and request body

--- firstserver/server.mjs
+++ firstserver/server.mjs
@@ -4,11 +4,16 @@ const app = express()
 const port = 3000
 // endpoints - APIs
 
+//body parser middleware 
+
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     
   res.send('<h1 style="color:blue">Learning Backend Development with ExpressJs!</h1>')
 })
+
 
 
 app.get('/products', (req, res) => {
@@ -19,6 +24,59 @@ app.get('/about', (req, res) => {
 
   res.status(404).send('<h1 style="color:blue">Not found!</h1>')
 })
+
+
+
+//6 - 10 - 2025
+//route parameters (compulsory)
+app.get('/product/:id', (req, res) => {
+
+  const id = req.params.id;
+  if (id > 100) {
+    res.status(404).json({message:`No product found`})
+    
+  } else {
+    
+    res.status(200).json({products:`showing details of product id : ${id}`})
+  }
+
+
+})
+//query parameters (optional)
+app.get('/products/search', (req, res) => {
+
+  const name = req.query.name;
+  if (!name){
+    res.status(200).json({message:`Hello User`})
+    
+  } else {
+    
+    res.status(200).json({message:`Hello ${name}`})
+  }
+
+})
+//request body
+app.get('/user', (req, res) => {
+const user  = req.body;
+// const {username, email,pass}  = req.body;
+// const username= req.body.username;
+// const username= req.body.username;
+// const username= req.body.username;
+
+// res.status(200).json({user:{
+//   username,email,pass
+// }})
+res.status(200).json({user:user})
+  
+
+})
+
+// Create 3 endpoints
+//  1. user details - implement request body
+// 2. delete user - implement route parameter 
+// 3. search user - implement query parameter (q) 
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
